4b747fee40c5ff6db76df685eb5d1b8a713ddc61
Atila Neves <aalvesne@cisco.com>
2013-05-15 16:12:58 +0200

    1st commit

---
+++ StaticRangeInt.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+#include <cassert>
+
+
+constexpr bool isInRange(int val, int low, int high) {
+     return val >= low && val < high;
+}
+
+//compile-time: fails to compile if val outside range
+//run-time: throws if val outside range
+constexpr bool getValInRange(int val, int low, int high) {
+    //The compile-time error can't include the values by language limitation
+    //The run-time error can't either due to the compile-time limitation
+    return isInRange(val, low, high) ?
+        val :
+        throw std::logic_error("Value outside range"); //throw can't compile
+}
+
+//open interval [low, high)
+template<int LOW, int HIGH>
+struct StaticRangeInt {
+    static_assert(HIGH > LOW, "Value outside range");
+    const int _val;    
+    constexpr StaticRangeInt(int val):_val(getValInRange(val, LOW, HIGH)) {
+    }
+};
+
+
+int readVal() {
+    std::cout << "Number to put in range? ";
+    int val;
+    std::cin >> val;
+    std::cout << std::endl;
+    return val;
+}
+
+int main() {
+    constexpr auto lo = 3, hi = 6;
+    const auto staticVal = 4; //usable at compile-time even w/o constexpr
+    //changing staticVal can produce a compile-time error for staticInt
+    constexpr StaticRangeInt<lo, hi> staticInt{staticVal};
+    const auto dynamicVal = readVal();
+    const StaticRangeInt<lo, hi> dynamicInt{dynamicVal}; //may throw
+
+    assert(staticInt._val  == staticVal);
+    assert(dynamicInt._val == dynamicVal);
+}
---
+++ StaticRangeInt.d
@@ -0,0 +1,39 @@
+#!/usr/bin/rdmd
+import std.stdio;
+import std.exception;
+import std.conv;
+
+
+auto isInRange(int val, int low, int high) {
+    return val >= low && val < high;
+}
+
+//open interval [low, high)
+struct StaticRangeInt(alias LOW, alias HIGH) if(HIGH > LOW) {
+    immutable int _val;
+    this(int val) {
+        enforce(isInRange(val, LOW, HIGH),
+                "Value must be in [" ~ to!string(LOW) ~ ", " ~ to!string(HIGH) ~ ")");
+        _val = val;
+    }
+}
+
+auto readVal() {
+    write("Number to put in range? ");
+    int val;
+    readf(" %d", &val);
+    writeln();
+    return val;
+}
+
+void main() {
+    immutable lo = 3, hi = 6;
+    immutable staticVal = 4;
+    //changing staticVal can produce a compile-time error for staticInt
+    static immutable auto staticInt = StaticRangeInt!(lo, hi)(staticVal);
+    immutable auto dynamicVal = readVal();
+    immutable auto dynamicInt = StaticRangeInt!(lo, hi)(dynamicVal);
+    
+    assert(staticInt._val  == staticVal);
+    assert(dynamicInt._val == dynamicVal);
+}
